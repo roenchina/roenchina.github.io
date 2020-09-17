390043d4dd582a959cf0c666a8071c67a6fcc0f3
yuruohan <1005623825@qq.com>
2020-09-17 14:06:45 +0800

    Init based on AOW

---
+++ myworks/data.js
@@ -0,0 +1,23 @@
+var myData={
+    designer:{
+        name:'My Portfolio',    // title
+        phone:'',
+        tag:[""],
+        bg:'#c8c8c8',
+        resume:'坐标玉泉',
+    },
+    gallery:[
+        {src:"myworks/1.JPG",w:2048,h:2048},
+        {src:"myworks/4.JPG",w:1080,h:1620},
+        {src:"myworks/2.JPG",w:2048,h:1368},
+        {src:"myworks/12.JPG",w:2048,h:1368},
+        {src:"myworks/7.JPG",w:1620,h:1080},
+        {src:"myworks/11.JPG",w:2048,h:1368},
+        {src:"myworks/3.JPG",w:2048,h:2048},
+        {src:"myworks/5.JPG",w:2048,h:1536},
+        {src:"myworks/6.JPG",w:2048,h:1368},
+        {src:"myworks/8.JPG",w:1368,h:2048},
+        {src:"myworks/10.JPG",w:1368,h:2048},
+        {src:"myworks/9.JPG",w:1368,h:2048},
+    ]
+}
